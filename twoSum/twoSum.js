165bc440be5103810a71f202e1c41df14243a262
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-09-29 12:47:05 -1000

    first commit

--- twoSum/twoSum.js
+++ twoSum/twoSum.js
@@ -11,3 +11,17 @@
 
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
+
+var twoSum =  function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i <nums.length; i++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if (map.has(num2)){
+            return [i, map.get(num2)];
+        }
+        map.set(num1, i)
+    }
+};
+
+console.log(twoSum(nums = [3,2,4], target = 6))
