9c76b54344daff228162231a4ba0450fa22d2297
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-09-30 15:19:06 -1000

    first commit

--- romanToInt/romanToInt.js
+++ romanToInt/romanToInt.js
@@ -15,6 +15,28 @@
 // The same principle applies to the number nine, which is written as IX. 
 // There are six instances where subtraction is used:
 
+var romeToInt = function(string) {
+    const romanMap = {
+        I:1,
+        V:5,
+        X:10,
+        L:50,
+        C:100,
+        D:500,
+        M:1000
+    }
+    let result = 0;
+    string.split('').forEach((num,i) => {
+        if(romanMap[num] < romanMap[string[i+1]]) result -= romanMap[num];
+        else result += romanMap[num];
+    });
+    return result
+}
+console.log(romeToInt("MCMXCIV"))
+console.log(romeToInt("III"))
+console.log(romeToInt("LVIII"))
+
+
 // Example 1
 
 // Input: s = "LVIII"
