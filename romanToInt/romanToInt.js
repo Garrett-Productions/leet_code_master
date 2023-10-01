853d822e0f97a645b953f75c244d6739de24e852
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-09-30 15:10:00 -1000

    first commit

--- romanToInt/romanToInt.js
+++ romanToInt/romanToInt.js
@@ -14,3 +14,16 @@
 // Because the one is before the five we subtract it making four. 
 // The same principle applies to the number nine, which is written as IX. 
 // There are six instances where subtraction is used:
+
+// Example 1
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+
+// Example 2
+
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
