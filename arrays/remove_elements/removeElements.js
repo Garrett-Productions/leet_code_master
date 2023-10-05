5f1a2b56b9de6340a88495356379cd8865c5a75a
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-05 10:01:40 -1000

    frist commit

---
+++ arrays/remove_elements/removeElements.js
@@ -0,0 +1,20 @@
+// Given an integer array nums and an integer val, 
+// remove all occurrences of val in nums in-place. 
+// The order of the elements may be changed. 
+// Then return the number of elements in nums which are not equal to val.
+
+// Consider the number of elements in nums which are not equal to val be k, 
+// to get accepted, you need to do the following things:
+
+var removeElement = function(nums, val) {
+    let uniqueVals = 0
+    for (let i = 0; i< nums.length; i++){
+        if(nums[i] !== val){ // if the current position does not equal value then increase our index
+            nums[uniqueVals] = nums[i];
+            uniqueVals++
+        }
+    }
+    return uniqueVals
+};
+console.log(removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
+
