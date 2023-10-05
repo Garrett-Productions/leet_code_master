44341ad71627cdf84c1ee13075c938a8115814a6
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-05 10:08:48 -1000

    first commit

--- arrays/remove_elements/removeElements.js
+++ arrays/remove_elements/removeElements.js
@@ -18,4 +18,5 @@ var removeElement = function(nums, val) {
 };
 console.log(removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
 console.log(removeElement(nums = [7,8,7,9,9,9,], val = 9))
+console.log(removeElement(nums = [7,8,7,9,9,9,], val = 9))
 
