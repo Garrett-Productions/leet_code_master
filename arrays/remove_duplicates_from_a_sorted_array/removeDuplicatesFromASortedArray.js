c33303ae5992b9178d32e4ffeadf23a5ab362e5c
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-05 09:31:08 -1000

    first commit

--- arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
+++ arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
@@ -22,3 +22,4 @@ var removeDuplicates = function(nums){
 };
 // Input: nums = [0,0,1,1,1,2,2,3,3,4]
 // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+console.log("Unique nums are:", removeDuplicates(nums = [0,0,1,1,1,2,2,3,3,4]))
