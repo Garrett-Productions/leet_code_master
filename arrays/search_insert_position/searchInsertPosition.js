1af75831f6b97a2dda95e617601aa67909504489
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-06 08:53:07 -1000

    first commit

--- arrays/search_insert_position/searchInsertPosition.js
+++ arrays/search_insert_position/searchInsertPosition.js
@@ -10,9 +10,21 @@ var searchInsert = function(nums, target){
     let right = nums.length - 1;
 
     while(left <= right){
-        let mid = left + Math.floor
+        let mid = left + Math.floor((right-left)/2);
+
+        if(nums[mid] === target){
+            return mid;
+        }
+        if(nums[mid] > target){
+            right = mid -1;
+        } else {
+            left = mid +1;
+        }
     }
+    return left;
 }
+
+console.log(searchInsert(nums = [1,3,5,6], target = 5))
 // Example 1:
 
 // Input: nums = [1,3,5,6], target = 5
