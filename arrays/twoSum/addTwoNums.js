815b9103fea0eed30cc873f6a53fe47dee3918b4
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-12 15:05:46 -1000

    first commit

--- arrays/twoSum/addTwoNums.js
+++ arrays/twoSum/addTwoNums.js
@@ -32,3 +32,25 @@ console.log(twoSum(array = [3,2,4], target = 6))
 // we create one variable to store our iteration value of the array..
 // we create another var to capture what we are looking for, being the target we are given minus the the first variable
 // if our mao has the value of num2, then return an array of the current index of i, and num2, through the map.get method
+
+
+// Given an array of integers, array, and an integer target, 
+// return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, 
+// and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+
+var twoSome = function(array, value){
+    let map = new Map()
+}
