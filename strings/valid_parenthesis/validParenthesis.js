9d210337228fec326dfd535ed4a8dc42f0d6134e
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 13:14:34 -1000

    first commit

--- strings/valid_parenthesis/validParenthesis.js
+++ strings/valid_parenthesis/validParenthesis.js
@@ -12,37 +12,37 @@
 
 
 // with Built ins 
-function isValid(s) {
-    const stack = [];
-    const openBrackets = '({[';
-    const closeBrackets = ')}]';
-    const bracketMap = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    };
+// function isValid(s) {
+//     const stack = [];
+//     const openBrackets = '({[';
+//     const closeBrackets = ')}]';
+//     const bracketMap = {
+//         ')': '(',
+//         '}': '{',
+//         ']': '['
+//     };
 
-    for (let i = 0; i < s.length; i++) {
-        const currentBracket = s[i];
-        if (openBrackets.includes(currentBracket)) {
-            stack.push(currentBracket);
-        } else if (closeBrackets.includes(currentBracket)) {
-            const topOfStack = stack.pop();
-            if (topOfStack !== bracketMap[currentBracket]) {
-                return false; // Invalid, mismatched brackets
-            }
-        } else {
-            return false; // Invalid character in the input
-        }
-    }
+//     for (let i = 0; i < s.length; i++) {
+//         const currentBracket = s[i];
+//         if (openBrackets.includes(currentBracket)) {
+//             stack.push(currentBracket);
+//         } else if (closeBrackets.includes(currentBracket)) {
+//             const topOfStack = stack.pop();
+//             if (topOfStack !== bracketMap[currentBracket]) {
+//                 return false; // Invalid, mismatched brackets
+//             }
+//         } else {
+//             return false; // Invalid character in the input
+//         }
+//     }
 
-    return stack.length === 0; // If stack is empty, all brackets are matched
-}
+//     return stack.length === 0; // If stack is empty, all brackets are matched
+// }
 
-// Example usage
-const inputString = '({[]})';
-const isValidString = isValid(inputString);
-console.log(isValidString); // Output: true
+// // Example usage
+// const inputString = '({[]})';
+// const isValidString = isValid(inputString);
+// console.log(isValidString); // Output: true
 
 // without built ins
 
