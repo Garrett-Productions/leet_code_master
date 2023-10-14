fb34f44fdce6494d732ab58a3652000b93ad644e
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 12:34:07 -1000

    first commit

--- strings/valid_parenthesis/validParenthesis.js
+++ strings/valid_parenthesis/validParenthesis.js
@@ -9,3 +9,35 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
+
+function isValid(s) {
+    const stack = [];
+    const openBrackets = '({[';
+    const closeBrackets = ')}]';
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const currentBracket = s[i];
+        if (openBrackets.includes(currentBracket)) {
+            stack.push(currentBracket);
+        } else if (closeBrackets.includes(currentBracket)) {
+            const topOfStack = stack.pop();
+            if (topOfStack !== bracketMap[currentBracket]) {
+                return false; // Invalid, mismatched brackets
+            }
+        } else {
+            return false; // Invalid character in the input
+        }
+    }
+
+    return stack.length === 0; // If stack is empty, all brackets are matched
+}
+
+// Example usage
+const inputString = '({[]})';
+const isValidString = isValid(inputString);
+console.log(isValidString); // Output: true
