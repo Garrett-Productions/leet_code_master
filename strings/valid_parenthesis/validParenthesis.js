e72a6701bc7ada9893111080e8597ed81db81a04
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 12:29:30 -1000

    first commit

--- strings/valid_parenthesis/validParenthesis.js
+++ strings/valid_parenthesis/validParenthesis.js
@@ -1,1 +1,11 @@
 // next up is Valid Parenthesis..
+
+// As if you were a senior software engineer in Javascript at Apple, solve this algorithm with best case time complexity in mind..
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
