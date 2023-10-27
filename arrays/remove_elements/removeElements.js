1aa52348f93b6214587ce5a38fcb36766c8e4e68
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-26 16:54:01 -1000

    millionth commit

--- arrays/remove_elements/removeElements.js
+++ arrays/remove_elements/removeElements.js
@@ -52,3 +52,27 @@ var removeCertainVal = function(arr,value){
 }
 console.log(removeCertainVal(nums = [7,8,7,9,9,9,], val = 9))
 console.log(removeCertainVal(nums = [0,1,2,2,3,0,4,2], val = 2))
+
+
+// Given an integer array nums and an integer val, 
+// remove all occurrences of val in nums in-place. 
+// The order of the elements may be changed. 
+// Then return the number of elements in nums which are not equal to val.
+
+
+function remove(numsArray, value){
+    // if value !== the value in the array increase counter
+    //what do we need to return, num of elements in numsArray that arent equal to value
+    // vars needed = counter, increasing every time the value in ther array doesnt equal the value in comparison
+    counter = 0
+    for(let i = 0; i <= numsArray.length-1; i++){
+        // console.log(i)
+        if(numsArray[i]!== value)
+        counter++
+    
+    }
+    console.log(counter)
+    return counter
+}
+
+remove(nums = [0,1,2,2,3,0,4,2], val = 2)
--- arrays/remove_elements/tempCodeRunnerFile.js
+++ arrays/remove_elements/tempCodeRunnerFile.js
@@ -1,3 +1,16 @@
-console.log(removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
-// console.log(removeElement(nums = [7,8,7,9,9,9,], val = 9))
-// console.log(removeElement(nums = [7,8,7,9,9,9,], val = 9))
+function remove(numsArray, value){
+    // if value !== the value in the array increase counter
+    //what do we need to return, num of elements in numsArray that arent equal to value
+    // vars needed = counter, increasing every time the value in ther array doesnt equal the value in comparison
+    counter = 0
+    for(let i = 0; i <= numsArray.length-1; i++){
+        // console.log(i)
+        if(numsArray[i]!== value)
+        counter++
+    
+    }
+    console.log(counter)
+    return counter
+}
+
+remove(nums = [0,1,2,2,3,0,4,2], val = 2)
