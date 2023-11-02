de8be14c9df64defdf62ff5d4cd96aed6b0057e0
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:37:30 -1000

    first commit

--- arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
+++ arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
@@ -36,3 +36,16 @@ var removeDupe = function(arr){
     return counter
 }
 console.log(removeDupe(nums = [0,0,1,1,1,2,2,3,3,4]))
+
+
+
+// Given an integer array nums sorted in non-decreasing order, 
+// remove the duplicates in-place such that each unique element appears only once. 
+// The relative order of the elements should be kept the same. 
+// Then return the number of unique elements in nums.
+
+
+
+
+
+console.log(removeDupees(nums = [0,0,1,1,1,2,2,3,3,4]))
