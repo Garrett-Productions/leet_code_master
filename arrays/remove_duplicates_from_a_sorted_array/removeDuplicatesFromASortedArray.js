4e3eee1ebefcdc12b05a0092428a25cdf6955801
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:46:17 -1000

    first commit

--- arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
+++ arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
@@ -48,12 +48,11 @@ console.log(removeDupe(nums = [0,0,1,1,1,2,2,3,3,4]))
 function removeDupees(nums){
     counter = 0
     for(let i = 0; i < nums.length; i++){
-        if(nums[i]!== nums[i+1]){
-            // console.log(i)
+        if(nums[i]!= nums[i+1]){
+            nums[counter] = nums[i]
             counter++
-            // if(i==='undefined'){
-            //     break
-            // }
+            console.log(counter)
+            
         }
     }
     return counter
--- arrays/remove_duplicates_from_a_sorted_array/tempCodeRunnerFile.js
+++ arrays/remove_duplicates_from_a_sorted_array/tempCodeRunnerFile.js
@@ -1,16 +1,16 @@
 function removeDupees(nums){
     counter = 0
     for(let i = 0; i < nums.length; i++){
-        if(nums[i]!== nums[i+1]){
-            console.log(i)
+        if(nums[i]!= nums[i+1]){
+            nums[counter] = nums[i]
             counter++
-            if(i==='undefined'){
-                break
-            }
+            console.log(counter)
+            // if(i==='undefined'){
+            //     break
+            // }
         }
     }
+    return counter
 }
-
-
 
 console.log(removeDupees(nums = [0,0,1,1,1,2,2,3,3,4]))
