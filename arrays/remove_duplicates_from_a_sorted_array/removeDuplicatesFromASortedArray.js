8302c55fd56b1eccecb1d0300ddd26cdc74c604d
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:42:27 -1000

    first commit

--- arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
+++ arrays/remove_duplicates_from_a_sorted_array/removeDuplicatesFromASortedArray.js
@@ -49,15 +49,14 @@ function removeDupees(nums){
     counter = 0
     for(let i = 0; i < nums.length; i++){
         if(nums[i]!== nums[i+1]){
-            console.log(i)
+            // console.log(i)
             counter++
             // if(i==='undefined'){
             //     break
             // }
         }
     }
+    return counter
 }
-
-
 
 console.log(removeDupees(nums = [0,0,1,1,1,2,2,3,3,4]))
