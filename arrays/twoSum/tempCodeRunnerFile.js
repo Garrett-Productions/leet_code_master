c25e80f6e57e22e9921be2f301b3b92df83015ea
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 15:00:13 -1000

    first commit

--- arrays/twoSum/tempCodeRunnerFile.js
+++ arrays/twoSum/tempCodeRunnerFile.js
@@ -1,2 +1,13 @@
-else
-        
+function twoNumbers(nums, target){
+    let map = new Map();
+    for(let i = 0;i <nums.length; i++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if (map.has(num2)){
+            console.log(map.get(num2))
+            return [i, map.get(num2)];
+        }
+        map.set(num1, i)
+    }
+}
+console.log(twoNumbers(nums=[4,6,10,54,77], target = 60))
--- arrays/twoSum/twoSum.js
+++ arrays/twoSum/twoSum.js
@@ -108,5 +108,14 @@ var twoSum = function(nums, target) {
 
 function twoNumbers(nums, target){
     let map = new Map();
+    for(let i = 0;i <nums.length; i++){
+        let num1 = nums[i];
+        let num2 = target - num1;
+        if (map.has(num2)){
+            console.log(map.get(num2))
+            return [i, map.get(num2)];
+        }
+        map.set(num1, i)
+    }
 }
 console.log(twoNumbers(nums=[4,6,10,54,77], target = 60))
