cc6b06eb48dbc64af137685215a9ef8ecc9b3cf6
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:51:20 -1000

    first commit

--- arrays/twoSum/twoSum.js
+++ arrays/twoSum/twoSum.js
@@ -100,3 +100,7 @@ var twoSum = function(nums, target) {
 //let hashMap = new Map()
 //takes in 2 params
 // map.has, map.get, map.set
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
