507e7505f78264d6ba384852f5164b7005133d23
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:55:37 -1000

    first commit

--- arrays/twoSum/twoSum.js
+++ arrays/twoSum/twoSum.js
@@ -104,3 +104,9 @@ var twoSum = function(nums, target) {
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
+
+
+function twoNumbers(nums, target){
+    
+}
+console.log(twoNumbers(nums=[4,6,10,54,77], target = 60))
