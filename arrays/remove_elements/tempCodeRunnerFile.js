41be46d25e6d0c35f0638a0865ea558caf1dd167
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:35:23 -1000

    first commit

--- arrays/remove_elements/tempCodeRunnerFile.js
+++ arrays/remove_elements/tempCodeRunnerFile.js
@@ -1,16 +1,10 @@
-function remove(numsArray, value){
-    // if value !== the value in the array increase counter
-    //what do we need to return, num of elements in numsArray that arent equal to value
-    // vars needed = counter, increasing every time the value in ther array doesnt equal the value in comparison
-    counter = 0
-    for(let i = 0; i <= numsArray.length-1; i++){
-        // console.log(i)
-        if(numsArray[i]!== value)
-        counter++
-    
-    }
-    console.log(counter)
-    return counter
-}
+function removeNums(nums, val){
 
-remove(nums = [0,1,2,2,3,0,4,2], val = 2)
+    counterToReturn = 0
+    for(let i = 0;i <=nums.length;i++){
+        if(nums[i] !== val)
+        counterToReturn++
+    }
+    console.log(counterToReturn)
+}
+removeNums(nums = [0,1,2,2,3,0,4,2], val = 2)
