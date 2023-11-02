353b0149f2bac5d579300b7cd9e85498a43f4d23
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-01 14:36:07 -1000

    first commit

--- arrays/remove_elements/removeElements.js
+++ arrays/remove_elements/removeElements.js
@@ -88,11 +88,11 @@ remove(nums = [0,1,2,2,3,0,4,2], val = 2)
 function removeNums(nums, val){
 
     counterToReturn = 0
-    for(let i = 0;i <=nums.length;i++){
+    for(let i = 0;i <=nums.length-1;i++){
         if(nums[i] !== val)
         console.log(i)
         counterToReturn++
     }
-    console.log(counterToReturn)
+    // console.log(counterToReturn)
 }
 removeNums(nums = [0,1,2,2,3,0,4,2], val = 2)
--- arrays/remove_elements/tempCodeRunnerFile.js
+++ arrays/remove_elements/tempCodeRunnerFile.js
@@ -3,8 +3,9 @@ function removeNums(nums, val){
     counterToReturn = 0
     for(let i = 0;i <=nums.length;i++){
         if(nums[i] !== val)
+        console.log(i)
         counterToReturn++
     }
-    console.log(counterToReturn)
+    // console.log(counterToReturn)
 }
 removeNums(nums = [0,1,2,2,3,0,4,2], val = 2)
