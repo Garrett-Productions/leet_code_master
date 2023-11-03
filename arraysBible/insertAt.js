5d8b3f1a166fa10eb47537e4d2ef2fa300752032
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-02 16:53:31 -1000

    first commit

--- arraysBible/insertAt.js
+++ arraysBible/insertAt.js
@@ -70,9 +70,9 @@ console.log(insertVal([1,2,5,6,10], 4, 11)) // 11 needs to go between 6 & 10 in 
 
 
 function insertValue(array, index, value){
-
+// we need to compare the length of the array with index,  if i is > index, set the next position equal to the current position, and take in the param index as an arguyement to my array and set it = to the value 
         for(let i = 0; i <= array.length; i++){
-            
+            //cond ti see uf 
         }
 }
 
