726b91984f844323061d05a69cd74ef84d4c640a
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-02 16:56:43 -1000

    first commit

--- arraysBible/insertAt.js
+++ arraysBible/insertAt.js
@@ -75,12 +75,10 @@ function insertValue(array, index, value){
 // and take in the param index as an arguyement to my array and set it = to the value 
         for(let i = array.length-1; i >= index; i--){ // starting we have i as 4
             console.log("array is", array,"iterator is", i)
-            
-            //cond to see uf 
+            // when i becomes equal to or greater than the index, we need to set the next position of i equal to the current one
     }
 
 }
-
 
 // at the 4th index of this array insert the value 11
 console.log(insertValue([1,2,5,6,10], 4, 11))
