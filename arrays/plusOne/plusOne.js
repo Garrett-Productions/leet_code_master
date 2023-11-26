90088f7fc441fc75d42c0931022c6a433109b961
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-26 13:32:33 -1000

    first commitz

--- arrays/plusOne/plusOne.js
+++ arrays/plusOne/plusOne.js
@@ -10,10 +10,11 @@
 //  add 1, turn it back into an array and return it.
 //Example
 
-Input: digits = [1,2,3]
-Output: [1,2,4]
-Explanation: The array represents the integer 123.
-Incrementing by one gives 123 + 1 = 124.
-Thus, the result should be [1,2,4].
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
 
-simply apply the toString() method on the given array
+// simply apply the toString() method on the given array
+// To convert a string to an integer parseInt(), 
