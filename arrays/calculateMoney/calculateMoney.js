1ab92f73400574113dc880c5fc0b7544f311ac6e
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-12-20 13:35:34 -0500

    first commit

--- arrays/calculateMoney/calculateMoney.js
+++ arrays/calculateMoney/calculateMoney.js
@@ -3,4 +3,8 @@
 // --deposit variable for incrementing during a loop
 // --ifMonday variable
 
-// Ways to solve:
+// -----Ways to solve:
+// loop through n, or the amount of days.
+// during our loop add the deposit to our total amount var
+// conditional, if it's monday,(modulus?), incrememnt ifMonday var by 1
+// also if its monday, set the deposit amount to newMon
