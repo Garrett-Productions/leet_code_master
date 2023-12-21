deb1145f3579213c5af568549c3322a56e1f990d
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-12-21 15:31:37 -0500

    first commit

--- arrays/calculateMoney/calculateMoney.js
+++ arrays/calculateMoney/calculateMoney.js
@@ -1,5 +1,5 @@
 // Variables needed: 
-// --output variable, or sum, which is total amount of $
+// --output variable, or total amount, wwhich is what we need to return
 // --deposit variable for incrementing during a loop
 // --ifMonday variable
 
@@ -9,23 +9,25 @@
 // loop through n, or the amount of days.
 // during our loop add the deposit to our total amount var
 // conditional, if it's monday,(modulus?), incrememnt ifMonday var by 1
+
 // also if its monday, set the deposit amount to newMon
 
 function totalMoney(n){
-    let sum = 0
+    let totalAmount = 0
     let deposit = 1
     let mondayDeposit = 1
-    
     for (let i = 1; i <= n; i++){
-        sum+=deposit 
+        console.log(i)
+        totalAmount+=deposit 
+        console.log("first deposit",deposit)
         deposit++
         if (i % 7 === 0){
             mondayDeposit++;
             deposit = mondayDeposit
+            console.log("after 2nd monday deposit", deposit)
         }
     }
-    console.log("My sum of money at the end of", n, "day is", sum)
-    return sum;
+    console.log("My total amount of money at the end of", n, "day is", totalAmount)
+    return totalAmount;
 }
-
 totalMoney(n=10)
--- arrays/calculateMoney/tempCodeRunnerFile.js
+++ arrays/calculateMoney/tempCodeRunnerFile.js
@@ -1,18 +1,19 @@
 function totalMoney(n){
-    let sum = 0
+    let totalAmount = 0
     let deposit = 1
     let mondayDeposit = 1
-    
     for (let i = 1; i <= n; i++){
-        sum+=deposit 
+        console.log(i)
+        totalAmount+=deposit 
+        console.log("first deposit",deposit)
         deposit++
         if (i % 7 === 0){
             mondayDeposit++;
             deposit = mondayDeposit
+            console.log("after 2nd monday deposit", deposit)
         }
     }
-    console.log("My sum of money at the end of", n, "day is", sum)
-    return sum;
+    console.log("My total amount of money at the end of", n, "day is", totalAmount)
+    return totalAmount;
 }
-
 totalMoney(n=10)
